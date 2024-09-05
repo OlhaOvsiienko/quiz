411a88f73de4f1b9b254477121c94501afe85b4d
OlhaOvsiienko <o.v.ovsiyenko@gmail.com>
2024-09-05 18:11:59 +0300

    delete folder

---
+++ src/components/quiz-content/QuizContent.js
@@ -0,0 +1,31 @@
+export const tests = [
+  {
+    question: "How many oceans are there in the world?",
+    answers: [4, 3, 2],
+    correct: 0,
+  },
+
+  {
+    question: "What is the capital of Peru?",
+    answers: ["Santiago", "Lima", "Montevideo"],
+    correct: 1,
+  },
+
+  {
+    question: "What continent is Nicaragua on?",
+    answers: ["Africa", "North America", "South America"],
+    correct: 1,
+  },
+
+  {
+    question: "What is the deepest river in the world?",
+    answers: ["Dnieper", "Nil", "Congo"],
+    correct: 2,
+  },
+
+  {
+    question: "What is the largest desert in the world?",
+    answers: ["Sahara", "Gobi", "Antarctic desert"],
+    correct: 2,
+  },
+];
